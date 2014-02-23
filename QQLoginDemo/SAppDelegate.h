22c7f1ca98ac21e306ff78a41ae2e440dda27a19
shi_dongdong <shi_dongdong@shi-dongdongdeMacBook-Pro.local>
2014-02-23 21:37:42 +0800

    Initial Commit

---
+++ QQLoginDemo/SAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAppDelegate.h
+//  QQLoginDemo
+//
+//  Created by shi_dongdong on 14-2-23.
+//  Copyright (c) 2014年 shi_dongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QQLoginDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QQLoginDemo
+//
+//  Created by shi_dongdong on 14-2-23.
+//  Copyright (c) 2014年 shi_dongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAppDelegate class]));
+    }
+}
